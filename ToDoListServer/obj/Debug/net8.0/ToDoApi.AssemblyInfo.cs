089eff264b7d60da76f5d479b950cd9234cc7215
Michal-Frishman <102004m@gmail.com>
2025-05-25 14:48:34 +0300

    change dotnet version

--- ToDoListServer/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
+++ ToDoListServer/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2479ac876220aa77045413a444a7013802e1ecc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a5b7352a1dac5d8df42283460f9f1fedd6c3b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
